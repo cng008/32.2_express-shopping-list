40dd8b68abdeb1b6d7680457f8c682e9049336cb
Christien Ng <ngchristien@gmail.com>
2022-04-18 18:10:33 -0700

    get more error details

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const itemsRoutes = require('./routes/items');
 const ExpressError = require('./expressError');
+const morgan = require('morgan');
+
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/items', itemsRoutes); // apply a prefix to every route in itemsRoutes
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
@@ -13,13 +16,14 @@ app.use((req, res, next) => {
 });
 
 // GENERIC ERROR HANDLER
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
   // the default status is 500 Internal Server Error
-  res.status(err.status || 500);
+  let status = error.status || 500;
+  let message = error.message;
+  // set the status and alert the user
+  return res.status(status).json({ error: { message, status } });
 
-  return res.json({
-    error: err.message
-  });
+  // res.status(error.status).send(error.msg) //alt
 });
 // END GENERIC ERROR HANDLER
 
